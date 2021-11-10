260f1b123c31762ffd45a45581a85adabe788071
Lindsay Ward <lward23@hotmail.com>
2021-11-10 11:55:34 -0800

    Added footer with links to open-source code and LinkedIn profile

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Search />
+      <footer>
+        <p>
+          <a
+            href="https://github.com/lindsaymward/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://www.linkedin.com/in/lindsaymward21/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Lindsay Ward
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
