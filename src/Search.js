00c10f8b51292c26ae4665fe5f10c4f0c4cfb5f1
Lindsay Ward <lward23@hotmail.com>
2021-11-21 18:15:34 -0800

    Update state for weather data to be one state via object instead of multiple states

--- src/Search.js
+++ src/Search.js
@@ -3,22 +3,17 @@ import axios from "axios";
 
 export default function Search() {
   let [cityInput, setCityInput] = useState("");
-  let [city, setCity] = useState("");
-  let [temp, setTemp] = useState("");
-  let [desc, setDesc] = useState("");
-  let [humidity, setHumidity] = useState("");
-  let [wind, setWind] = useState("");
-  let [iconUrl, setIcon] = useState("");
+  let [weatherData, setWeatherData] = useState({});
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=5df8b506b715f17ed0c74fd6fd849642&units=metric`;
   function getWeather(response) {
-    setCity(response.data.name);
-    setTemp(Math.round(response.data.main.temp));
-    setDesc(response.data.weather[0].description);
-    setHumidity(response.data.main.humidity);
-    setWind(Math.round(response.data.wind.speed));
-    setIcon(
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
+    setWeatherData({
+      city: response.data.name,
+      temp: Math.round(response.data.main.temp),
+      desc: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: Math.round(response.data.wind.speed),
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -49,7 +44,7 @@ export default function Search() {
       </form>
     </div>
   );
-  if (temp === "") {
+  if (weatherData.temp === "") {
     return searchForm;
   } else {
     return (
@@ -57,19 +52,19 @@ export default function Search() {
         {searchForm}
         <ul>
           <li>
-            <h1>{city}</h1>
+            <h1>{weatherData.city}</h1>
           </li>
           <li>
-            <span className="temp">{temp}</span>
+            <span className="temp">{weatherData.temp}</span>
             <span className="unit">°C</span>
           </li>
           <span className="conditions">
-            <li>Humidity: {humidity}%</li>
-            <li>Wind: {wind}km/h</li>
-            <li className="capitalize">{desc}</li>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind}km/h</li>
+            <li className="capitalize">{weatherData.desc}</li>
           </span>
           <li>
-            <img src={iconUrl} alt="Weather icon" />
+            <img src={weatherData.iconUrl} alt="Weather icon" />
           </li>
         </ul>
       </div>
